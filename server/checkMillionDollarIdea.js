20a5f30e2edb2313e50c4db1d262febbb7c955ca
Emma Secrest <emma@Emmas-MBP.attlocal.net>
2021-12-02 15:09:42 -0500

    check million dollar idea completed

--- server/checkMillionDollarIdea.js
+++ server/checkMillionDollarIdea.js
@@ -2,8 +2,10 @@ const checkMillionDollarIdea = (req,res,next) => {
     const {numWeeks , weeklyRevenue } = req.body
     const totalRev  = Number(numWeeks) * Number(weeklyRevenue)
 
-    if( totalRev < 1000000 || !weeklyRevenue || !numWeeks || typeof totalRev === String || typeof numWeeks === String) {
+    if( totalRev < 1000000 || !weeklyRevenue || !numWeeks ) {
         res.status(400).send('Total yields less than 1 Million')
+    } else if( isNaN(totalRev)){
+        res.status(400).send()
     } else {
         next()
     }
