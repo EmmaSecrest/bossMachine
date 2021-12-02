9e737151716847a51de10b3553ad9cf400ce648d
Emma Secrest <emma@Emmas-MBP.attlocal.net>
2021-12-02 14:26:34 -0500

    get single idea complete

--- server/ideas.js
+++ server/ideas.js
@@ -15,6 +15,7 @@ ideasRouter.param('ideaId' , (req,res,next,id) => {
     const requestedIdea = getFromDatabaseById('ideas' , id)
     if(requestedIdea) {
         req.idea = requestedIdea
+        next();
     } else {
         res.status(404).send()
     }
@@ -24,6 +25,11 @@ ideasRouter.param('ideaId' , (req,res,next,id) => {
 ideasRouter.get('/' , (req,res,next) => {
     const allIdeas = getAllFromDatabase('ideas')
     res.status(200).send(allIdeas)
+})
+
+// stealing a single idea 
+ideasRouter.get("/:ideaId" , (req,res,next) => {
+    res.send(req.idea)
 })
 
 
